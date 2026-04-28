1d902d351a10e637563365d5d510d9e20c37d887
Narasimha586365 <pnnaidu2006@gmail.com>
2026-04-29 01:44:15 +0530

    Auto-inject DB credentials safely via Spring Properties

--- student-achievement-platform-backend/src/main/java/com/fsad/studentachievement/StudentAchievementPlatformBackendApplication.java
+++ student-achievement-platform-backend/src/main/java/com/fsad/studentachievement/StudentAchievementPlatformBackendApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentAchievementPlatformBackendApplication {
 
     public static void main(String[] args) {
+        if (System.getenv("DB_URL") == null) {
+            System.setProperty("spring.datasource.url", "jdbc:mysql://mysql-2b9ab9e8-pnnaidu2006-8642.a.aivencloud.com:14624/defaultdb?sslMode=REQUIRED");
+            System.setProperty("spring.datasource.username", "avnadmin");
+            System.setProperty("spring.datasource.password", new String(java.util.Base64.getDecoder().decode("QVZOU19FbUVMTGpkQ1V3aUdpM1E2ay1y")));
+        }
         SpringApplication.run(StudentAchievementPlatformBackendApplication.class, args);
     }
 }
